cf60f33852d8e5bd854e2da81d57e6ae9e66a4e1
Santiago Finamore <76710757+Finamore00@users.noreply.github.com>
2023-04-10 11:56:58 -0400

    Agregado contador de archivos, filtro de input y busqueda por nombre

--- main.c
+++ main.c
@@ -39,7 +39,7 @@
  * ./fameChecker [-r <region>] [-s <species>] [-t <type>] [-c|--nocount] [-l|--list] [-s|--size] [name]
  *
  * En donde 
- *  -r              indica la region (johto, janto y orange_islands).
+ *  -r              indica la region (johto, kanto y orange_islands).
  *  -s              indica la especia a buscar (pokemon, trainers).
  *  -t              indica el tipo de aparicion (main, one_time, recurring).
  *  -c|--nocount    indica que debe aparecer el numero de archivos encontrado.
@@ -68,9 +68,10 @@
 char* get_filters_files(char* type, int argc, char **argv);
 int get_filters_output(char* type, int argc, char **argv);
 int traverse_directory(char* dirname, int indent,
-    char* r, char* sp, char* t,
+    char* r, char* sp, char* t, char *name,
     int c, int l, int sz,
-    int r_ch, int sp_ch, int t_ch);
+    int r_ch, int sp_ch, int t_ch, int *counter);
+int verify_input(char *region, char *species, char *type);
 int is_directory(char* path);
 int is_reg_file(char* path);
 int calc_size(char* path);
@@ -78,8 +79,7 @@ int calc_size(char* path);
 int main(int argc, char **argv) {
     /* Variable para los filtros de la query a realizar*/
     char *region, *specie, *type, *name;
-    int nocount, list, size;
-
+    int nocount, list, size, count, arg_count;
     region = get_filters_files("-r", argc, argv);
     specie = get_filters_files("-s", argc, argv);
     type = get_filters_files("-t", argc, argv);
@@ -93,11 +93,8 @@ int main(int argc, char **argv) {
     list = (list == 1) ? list : get_filters_output("--list", argc, argv);
     size = (size == 1) ? size : get_filters_output("--size", argc, argv);
 
-    traverse_directory("./directorio", 1, region, specie, type, nocount, list, size, 0, 0, 0);
-
     /* se verifica que la region ingresada sea valida*/
-    /* TO DO: funcion verify input */
-    /*if (!verify_input(region, specie, type)) {
+    if (verify_input(region, specie, type)) {
         printf("Error in input command.\n");
         printf("Usage: ./fameChecker [-r <region>] [-s <species>] [-t <type>] [-c|--nocount] [-l|--list] [-s|--size] [name]\n");
         printf("<region> = [johto, kanto, orange_islands]\n");
@@ -105,8 +102,47 @@ int main(int argc, char **argv) {
         printf("<type> = [main, one_time, recurring, gym_leaders]\n");
 
         exit(1);
-    }*/
+    }
 
+    /*Contabilizacion de argumentos*/
+    arg_count = 0;
+    arg_count += 2*(strcmp(region, "") != 0);
+    arg_count += 2*(strcmp(specie, "") != 0);
+    arg_count += 2*(strcmp(type, "") != 0);
+    arg_count += nocount + list + size;
+
+    /*Se compara con la cantida de argumentos ingresados en consola*/
+    if (arg_count < argc - 1) {
+        /*Si sobra algún argumento es que el usuario insertó un nombre a buscar*/
+        name = argv[argc-1];
+    } else {
+        name = ""; /*Si no se ingresó nada se deja un string vacío*/
+    }
+
+    traverse_directory("./directorio", 1, region, specie, type, name, nocount, list, size, 0, 0, 0, &count);
+    !nocount && printf("Number of files matching criteria: %d\n", count);
+
+    return 0;
+}
+
+/**
+ * Funcion que recibe el input ingresado al programa y verifica que sea valido para el
+ * programa. En caso de que el input ingresado sea valido se retorna 0 y se retorna 1 
+ * en caso contrario.
+ * 
+ * Entrada:
+ *      - input: apuntador al arreglo de strings que representa el input
+ * Salida: 0 si el input ingresado es valido y 1 en caso contrario
+*/
+
+int verify_input(char *region, char *species, char *type) {
+    if (strcmp(region, "kanto") && strcmp(region, "johto") && strcmp(region, "orange_islands") && strcmp(region, "")) {
+        return 1;
+    }else if (strcmp(species, "pokemon") && strcmp(species, "trainers") && strcmp(species, "")) {
+        return 1;
+    }else if (strcmp(type, "main") && strcmp(type, "one_time") && strcmp(type, "recurring") && strcmp(type, "gym_leaders") && strcmp(type, "")) {
+        return 1;
+    }
     return 0;
 }
 
@@ -224,9 +260,9 @@ void print_paths(int l, int sz, char* d_name, int indent, int size) {
 }
 
 int traverse_directory(char* dirname, int indent,
-    char* r, char* sp, char* t,
+    char* r, char* sp, char* t, char *name,
     int c, int l, int sz,
-    int r_ch, int sp_ch, int t_ch) {
+    int r_ch, int sp_ch, int t_ch, int *counter) {
 
     /**
      * Entrada:
@@ -245,7 +281,6 @@ int traverse_directory(char* dirname, int indent,
     */
     DIR* dir = opendir(dirname);
     struct dirent* entry;
-    int num_files = 0;
 
     if (!dir) return -1;
 
@@ -268,14 +303,14 @@ int traverse_directory(char* dirname, int indent,
                 if (!r_ch) {
                     if (!strcmp(d_name, r) || !strcmp(r, "")) {
                         print_paths(l, 0, d_name, indent, 0);
-                        traverse_directory(path, new_indent, r, sp, t, c, l, sz, 1, 0, 0);
+                        traverse_directory(path, new_indent, r, sp, t, name, c, l, sz, 1, 0, 0, counter);
                     }
                 }
                 if (!sp_ch) {
                     if (strstr(path, r)) {
                         if (!strcmp(d_name, sp) || !strcmp(sp, "")) {
                             print_paths(l, 0, d_name, indent, 0);
-                            traverse_directory(path, new_indent, r, sp, t, c, l, sz, 1, 1, 0);
+                            traverse_directory(path, new_indent, r, sp, t, name, c, l, sz, 1, 1, 0, counter);
                         }
                     }
                 }
@@ -283,14 +318,26 @@ int traverse_directory(char* dirname, int indent,
                     if (strstr(path, sp)) {
                         if (!strcmp(d_name, t) || !strcmp(t, "")) {
                             print_paths(l, 0, d_name, indent, 0);
-                            traverse_directory(path, new_indent, r, sp, t, c, l, sz, 1, 1, 1);
+                            traverse_directory(path, new_indent, r, sp, t, name, c, l, sz, 1, 1, 1, counter);
                         }
                     }
                 }
             }
 
-            if (is_reg_file(path) && l && t_ch) {
-                print_paths(l, sz, d_name, indent, calc_size(path));
+            if (is_reg_file(path) && t_ch) {
+                if (strcmp(name, "")) {
+                    if (strstr(d_name, name) != NULL) {
+                        *counter += 1;
+                        if (l) {
+                            print_paths(l, sz, d_name, indent, calc_size(path));
+                        }
+                    }
+                }else {
+                    *counter += 1;
+                    if (l) {
+                        print_paths(l, sz, d_name, indent, calc_size(path));
+                    }
+                }
             }
         }
     }
